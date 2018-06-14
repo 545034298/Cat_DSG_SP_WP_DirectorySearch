906eacd91a925de0e5000fbace95bae6ab81144c
Roger Zhang <zhangqinyuan2013@live.cn>
2018-06-14 11:33:12 +0800

    Initialize Employee Directory Web Part

---
+++ src/webparts/catDsgSpWp1005EmployeeDirectory/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICatDsgSpWp1005EmployeeDirectoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CatDsgSpWp1005EmployeeDirectoryWebPartStrings' {
+  const strings: ICatDsgSpWp1005EmployeeDirectoryWebPartStrings;
+  export = strings;
+}
